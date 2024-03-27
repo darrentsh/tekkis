167a3ecdcf1ba8843fcc01f3190c6590a0f14f75
Darren Ting <darren@mlperformance.co.uk>
2024-03-28 00:08:56 +0800

    0327 commit

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from main import scrape_cars
 from api import API
 
 if __name__ == "__main__":
-    # scrape_cars(1, 4)
+    scrape_cars(1, 4)
     api = API()
     api.app.run(debug=True, use_reloader=False, port=9874)
