c2130ac8ad4f4f3876e13b56d6146ff6bc1f3c81
Darren Ting <darren@mlperformance.co.uk>
2024-04-12 21:56:58 +0800

    update: remove test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-
-import mysql.connector
-mydb = mysql.connector.connect(
-            host='localhost', user="LAPTOP-AO8BAO92", password="kyoebrn98"
-        )
-mycursor = mydb.cursor()
-print(mycursor)
